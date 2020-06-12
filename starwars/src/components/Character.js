168278d60e1ff1e6888b89dafe0b7101ffa2b358
ViridityMoon <viriditymoon@gmail.com>
2020-06-12 12:36:27 -0400

    dependencies and basic card layout

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React from 'react';
+
+const Character = () => {
+    
+  
+    return (
+      <div className = 'character-card'>
+        <h2>Name</h2>
+        <img alt = 'pokemon'></img>
+      </div>
+    );
+}
+  
+  export default Character;
+  
